33e0f5fd69ba4680b3b148903f76cfabf146e724
Domosayshello <dominique.arno@gmail.com>
2023-08-29 18:45:36 +0000

    display company name on info page

--- info.js
+++ info.js
@@ -1,1 +1,35 @@
 const apiKey = myApiKey;
+
+
+const displayTickerInfo = async(ticker) => {
+    const infoUrl = `https://api.polygon.io/v3/reference/tickers/${ticker}?apiKey=${apiKey}`;
+    const res = await fetch(infoUrl);
+    const data = await res.json();
+    const results = data.results;
+    console.log(results)
+    const name = results.name;
+    const icon = `${results.branding.icon_url}?${apiKey}`;
+    const homepageLink = results.homepage_url;
+    const location = `${results.address.city}, ${results.address.state}`;
+    const description = results.description;
+    
+    const h2 = document.createElement('h2');
+    h2.textContent = name;
+    document.body.append(h2)
+}
+
+displayTickerInfo('ABNB')
+
+// document.querySelector("#search-form").addEventListener("submit", async(e) => {
+//   e.preventDefault();
+//   const ticker = e.target[0].value;
+//   const infoUrl = `https://api.polygon.io/v3/reference/tickers/${ticker}?apiKey=${apiKey}`;
+//   const res = await fetch(infoUrl);
+//   const data = await res.json();
+//   console.log(data);
+//   const results = data.results;
+//   const name = results.name;
+//   const icon = `${results.branding.iconurl}?${apiKey}`
+//   const homepage = `${}`
+  
+// });
